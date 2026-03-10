93c361db8343419c4a8922f718a685953bb3760a
deangoldhill <deangoldhill@gmail.com>
2026-03-10 19:39:03 +0000

    Update sensor.py

--- custom_components/dir_monitor/sensor.py
+++ custom_components/dir_monitor/sensor.py
@@ -1,67 +1,23 @@
 import logging
-from datetime import timedelta
-import voluptuous as vol
 
-from homeassistant.components.sensor import SensorEntity, PLATFORM_SCHEMA
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, CoordinatorEntity, UpdateFailed
-import homeassistant.helpers.config_validation as cv
-from homeassistant.const import CONF_HOST, CONF_API_KEY
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+from .const import DOMAIN, CONF_HOST
 
 _LOGGER = logging.getLogger(__name__)
 
-CONF_SERVERS = "servers"
-CONF_UPDATE_INTERVAL = "update_interval"
-
-SERVER_SCHEMA = vol.Schema({
-    vol.Required(CONF_HOST): cv.string,
-    vol.Required(CONF_API_KEY): cv.string,
-})
-
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_SERVERS): vol.All(cv.ensure_list, [SERVER_SCHEMA]),
-    vol.Optional(CONF_UPDATE_INTERVAL, default=300): cv.positive_int,
-})
-
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    servers = config[CONF_SERVERS]
-    update_interval = config[CONF_UPDATE_INTERVAL]
-    session = async_get_clientsession(hass)
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Set up the sensor platform via Config Flow."""
+    coordinator = hass.data[DOMAIN][entry.entry_id]
+    host = entry.data[CONF_HOST]
 
     entities = []
+    if coordinator.data:
+        for directory in coordinator.data.keys():
+            entities.append(DirMonitorSensor(coordinator, host, directory))
 
-    for server in servers:
-        host = server[CONF_HOST]
-        api_key = server[CONF_API_KEY]
-        url = f"http://{host}:7112/api/stats"
-
-        def create_fetch_method(fetch_url, fetch_key):
-            async def async_fetch_data():
-                try:
-                    async with session.get(fetch_url, headers={"X-API-Key": fetch_key}, timeout=10) as response:
-                        response.raise_for_status()
-                        data = await response.json()
-                        return {item["directory"]: item for item in data}
-                except Exception as err:
-                    raise UpdateFailed(f"Error communicating with {fetch_url}: {err}")
-            return async_fetch_data
-
-        coordinator = DataUpdateCoordinator(
-            hass,
-            _LOGGER,
-            name=f"dir_monitor_{host}",
-            update_method=create_fetch_method(url, api_key),
-            update_interval=timedelta(seconds=update_interval),
-        )
-
-        await coordinator.async_config_entry_first_refresh()
-
-        if coordinator.data:
-            for directory in coordinator.data.keys():
-                entities.append(DirMonitorSensor(coordinator, host, directory))
-
-    if entities:
-        async_add_entities(entities)
+    async_add_entities(entities)
 
 class DirMonitorSensor(CoordinatorEntity, SensorEntity):
     def __init__(self, coordinator, host, directory):
@@ -78,7 +34,7 @@ class DirMonitorSensor(CoordinatorEntity, SensorEntity):
     def device_info(self):
         """Link this entity to a device based on the host IP."""
         return {
-            "identifiers": {("dir_monitor", self._host)},
+            "identifiers": {(DOMAIN, self._host)},
             "name": f"Linux Server ({self._host})",
             "manufacturer": "Directory Monitor",
             "model": "Debian API Node",
